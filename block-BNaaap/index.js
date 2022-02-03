6a4866574133eb48752246554b0ccb663e3da9fd
Shazia Khan <shaziya5416@gmail.com>
2022-02-03 10:48:34 +0530

    changes

--- block-BNaaap/index.js
+++ block-BNaaap/index.js
@@ -15,6 +15,7 @@ var http= require(`http`);
 var server=http.createServer(handleRequest);
 
 function handleRequest(req,res){
+  console.log(req.method);
   res.statusCode=201;
   res.setHeader(`Content-Type`,`text/html`);
   res.end(`<h1>Welcome!<h1>`);
